58cf6a48ccc516416620a67f764b61820392ea0c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-02-27 00:45:15 +0000

    build based on 535e583

--- dev/search_index.js
+++ dev/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Introduction",
     "title": "Functionality",
     "category": "section",
-    "text": "The functionality of DrWatson is composed of the following core parts, each being independent from each other:Project Setup : A universal project structure and functions that allow you to consistently and robustly navigate through your project, no matter where it is located on your hard drive.\nHandling Simulations : A robust scheme for saving your data, naming files, finding out if a simulation already exists, producing tables of existing simulations/data.This core functionality is also demonstrated in the Real World Examples page in various examples. All of these examples are directly copied from code of real scientific projects that use DrWatson."
+    "text": "The functionality of DrWatson is composed of the following core parts, each being independent from each other:Project Setup : A universal project structure and functions that allow you to consistently and robustly navigate through your project, no matter where it is located on your hard drive.\nNaming & Saving Simulations : A robust scheme for saving your data, naming files, finding out if a simulation already exists, tagging the git commit ID, and more.\nRunning & Listing Simulations: Tools for producing tables of existing simulations/data, adding runs to such tables, preparing batch parameter containers, and more.Applications of DrWatson are demonstrated the Real World Examples page. All of these examples are directly copied from code of real scientific projects that use DrWatson."
 },
 
 {
@@ -154,23 +154,23 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#",
-    "page": "Handling Simulations",
-    "title": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
+    "title": "Naming & Saving Simulations",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "savenames/#Handling-Simulations-1",
-    "page": "Handling Simulations",
-    "title": "Handling Simulations",
+    "location": "savenames/#Naming-and-Saving-Simulations-1",
+    "page": "Naming & Saving Simulations",
+    "title": "Naming & Saving Simulations",
     "category": "section",
     "text": "This page discusses numerous tools that make life easier for handling simulations. Most (if not all) of these tools are also used in the examples demonstrated in the Real World Examples page. After reading the proper documentation here it might be worth it to have a look there as well!"
 },
 
 {
     "location": "savenames/#DrWatson.savename",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.savename",
     "category": "function",
     "text": "savename([prefix,], c [, suffix]; kwargs...)\n\nCreate a shorthand name, commonly used for saving a file, based on the parameters in the container c (Dict, NamedTuple or any other Julia composite type, e.g. created with Parameters.jl). If provided use the prefix and end the name with .suffix (i.e. you don\'t have to include the . in your suffix).\n\nThe function chains keys and values into a string of the form:\n\nkey1=val1_key2=val2_key3=val3\n\nwhile the keys are always sorted alphabetically. If you provide the prefix/suffix the function will do:\n\nprefix_key1=val1_key2=val2_key3=val3.suffix\n\nassuming you chose the default connector, see below. Notice that prefix can be any path and in addition if it ends as a path (/ or \\) then the connector is ommited.\n\nsavename can be very conveniently combined with @dict or @ntuple.\n\nKeywords\n\nallowedtypes = default_allowed(c) : Only values of type subtyping anything in allowedtypes are used in the name. By default this is (Real, String, Symbol).\naccesses = allaccess(c) : You can also specify which specific keys you want to use with the keyword accesses. By default this is all possible keys c can be accessed with, see allaccess.\ndigits = 3 : Floating point values are rounded to digits. In addition if the following holds:\nround(val; digits = digits) == round(Int, val)\nthen the integer value is used in the name instead.\nconnector = \"_\" : string used to connect the various entries.\n\nExamples\n\nd = (a = 0.153456453, b = 5.0, mode = \"double\")\nsavename(d; digits = 4) == \"a=0.1535_b=5_mode=double\"\nsavename(\"n\", d) == \"n_a=0.153_b=5_mode=double\"\nsavename(\"n/\", d) == \"n/a=0.153_b=5_mode=double\"\nsavename(d, \"n\") == \"a=0.153_b=5_mode=double.n\"\nsavename(\"data/n\", d, \"n\") == \"data/n_a=0.153_b=5_mode=double.n\"\nsavename(\"n\", d, \"n\"; connector = \"-\") == \"n-a=0.153-b=5-mode=double.n\"\nsavename(d, allowedtypes = (String,)) == \"mode=double\"\n\nrick = (never = \"gonna\", give = \"you\", up = \"!\");\nsavename(rick) == \"give=you_never=gonna_up=!\" # keys are sorted!\n\n\n\n\n\n"
@@ -178,7 +178,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.@dict",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.@dict",
     "category": "macro",
     "text": "@dict vars...\n\nCreate a dictionary out of the given variables that has as keys the variable names and as values their values.\n\nExamples\n\njulia> ω = 5; χ = \"test\"; ζ = π/3;\n\njulia> @dict ω χ ζ\nDict{Symbol,Any} with 3 entries:\n  :ω => 5\n  :χ => \"test\"\n  :ζ => 1.0472\n\n\n\n\n\n"
@@ -186,7 +186,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.@strdict",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.@strdict",
     "category": "macro",
     "text": "@strdict vars...\n\nSame as @dict but the key type is String.\n\n\n\n\n\n"
@@ -194,7 +194,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.@ntuple",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.@ntuple",
     "category": "macro",
     "text": "@ntuple vars...\n\nCreate a NamedTuple out of the given variables that has as keys the variable names and as values their values.\n\nExamples\n\njulia> ω = 5; χ = \"test\"; ζ = 3.14;\n\njulia> @ntuple ω χ ζ\n(ω = 5, χ = \"test\", ζ = 3.14)\n\n\n\n\n\n"
@@ -202,7 +202,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.ntuple2dict",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.ntuple2dict",
     "category": "function",
     "text": "ntuple2dict(nt) -> dict\n\nConvert a NamedTuple to a dictionary.\n\n\n\n\n\n"
@@ -210,7 +210,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.dict2ntuple",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.dict2ntuple",
     "category": "function",
     "text": "dict2ntuple(dict) -> ntuple\n\nConvert a dictionary (with Symbol or String as key type) to a NamedTuple.\n\n\n\n\n\n"
@@ -218,7 +218,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#Naming-Schemes-1",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "Naming Schemes",
     "category": "section",
     "text": "A robust naming scheme allows you to create quick names for simulations, create lists of simulations, check existing simulations, etc. More importantly it allows you to easily read and write simulations using a consistent naming scheme.savename\n@dict\n@strdict\n@ntupleNotice that this naming scheme integrates perfectly with Parameters.jl.Two convenience functions are also provided to easily switch between named tuples and dictionaries:ntuple2dict\ndict2ntuple"
@@ -226,7 +226,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.allaccess",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.allaccess",
     "category": "function",
     "text": "allaccess(c)\n\nReturn all the keys c can be accessed using access. For dictionaries/named tuples this is keys(c), for everything else it is fieldnames(typeof(c)).\n\n\n\n\n\n"
@@ -234,7 +234,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.access",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.access",
     "category": "function",
     "text": "access(c, key)\n\nAccess c with given key. For AbstractDict this is getindex, for anything else it is getproperty.\n\naccess(c, keys...)\n\nWhen given multiple keys, access is called recursively, i.e. access(c, key1, key2) = access(access(c, key1), key2) and so on. For example, if c is a NamedTuple then access(c, k1, k2) == ntuple.k1.k2.\n\nnote: Note\nPlease only extend the single key method when customizing access for your own Types.\n\n\n\n\n\n"
@@ -242,7 +242,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.default_allowed",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.default_allowed",
     "category": "function",
     "text": "default_allowed(c) = (Real, String, Symbol)\n\nReturn the (super-)Types that will be used as allowedtypes in savename or other similar functions.\n\n\n\n\n\n"
@@ -250,7 +250,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.default_prefix",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.default_prefix",
     "category": "function",
     "text": "default_prefix(c) = \"\"\n\nReturn the prefix that will be used by default in savename or other similar functions.\n\n\n\n\n\n"
@@ -258,7 +258,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#Customizing-savename-1",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "Customizing savename",
     "category": "section",
     "text": "You can customize savename for your own Types. For example you could make it so that it only uses some specific keys instead of all of them, only specific types, or you could make it access data in a different way (maybe even loading files!). You can even make it have a custom prefix!To do that you may extend the following functions:DrWatson.allaccess\nDrWatson.access\nDrWatson.default_allowed\nDrWatson.default_prefix"
@@ -266,7 +266,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.current_commit",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.current_commit",
     "category": "function",
     "text": "current_commit(path = projectdir()) -> commit\n\nReturn the current active commit id of the Git repository present in path, which by default is the project path. If the repository is dirty when this function is called the string will end with \"_dirty\".\n\nSee also tag!.\n\nExamples\n\njulia> current_commit()\n\"96df587e45b29e7a46348a3d780db1f85f41de04\"\n\njulia> current_commit(path_to_dirty_repo)\n\"3bf684c6a115e3dce484b7f200b66d3ced8b0832_dirty\"\n\n\n\n\n\n"
@@ -274,7 +274,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#DrWatson.tag!",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "DrWatson.tag!",
     "category": "function",
     "text": "tag!(d::Dict, path = projectdir()) -> d\n\nTag d by adding an extra field commit which will have as value the current_commit of the repository at path (by default the project\'s path). Does not operate if a key commit already exists.\n\nNotice that if String is not a subtype of the value type of d then a new dictionary is created and returned. Otherwise the operation is inplace (and the dictionary is returned again).\n\nExamples\n\njulia> d = Dict(:x => 3, :y => 4)\nDict{Symbol,Int64} with 2 entries:\n  :y => 4\n  :x => 3\n\njulia> tag!(d)\nDict{Symbol,Any} with 3 entries:\n  :y      => 4\n  :commit => \"96df587e45b29e7a46348a3d780db1f85f41de04\"\n  :x      => 3\n\n\n\n\n\n"
@@ -282,7 +282,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#Tagging-a-run-using-Git-1",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "Tagging a run using Git",
     "category": "section",
     "text": "For reproducibility reasons (and also to not go insane when asking \"HOW DID I GET THOSE RESUUUULTS\") it is useful to \"tag!\" any simulation/result/process with the Git commit of the repository.To this end there are two functions that can be used to ensure reproducibility:current_commit\ntag!Please notice that tag! will operate in place only when possible. If not possible then a new dictionary is returned. Also (importantly) these functions will never error as they are most commonly used when saving simulations and this could risk data not being saved."
@@ -290,50 +290,66 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "savenames/#Automatic-Tagging-during-Saving-1",
-    "page": "Handling Simulations",
+    "page": "Naming & Saving Simulations",
     "title": "Automatic Tagging during Saving",
     "category": "section",
     "text": "WIP. (adding the tag! functionality automatically with a save call)"
 },
 
 {
-    "location": "savenames/#DrWatson.dict_list",
-    "page": "Handling Simulations",
+    "location": "savenames/#Produce-or-Load-1",
+    "page": "Naming & Saving Simulations",
+    "title": "Produce or Load",
+    "category": "section",
+    "text": "WIP. (loading a simulation or producing it if it doesn\'t exist)"
+},
+
+{
+    "location": "addrun/#",
+    "page": "Running & Listing Simulations",
+    "title": "Running & Listing Simulations",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "addrun/#Running-and-Listing-Simulations-1",
+    "page": "Running & Listing Simulations",
+    "title": "Running & Listing Simulations",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "addrun/#DrWatson.dict_list",
+    "page": "Running & Listing Simulations",
     "title": "DrWatson.dict_list",
     "category": "function",
     "text": "dict_list(c)\n\nExpand the dictionary c into a vector of dictionaries. Each entry has a unique combination from the product of the Vector values of the dictionary while the non-Vector values are kept constant for all possibilities. The keys of the entries are the same.\n\nWhether the values of c are iterable or not is of no concern; the function considers as \"iterable\" only subtypes of Vector.\n\nUse the function dict_list_count to get an estimate of how many dictionaries will dict_list produce.\n\nExamples\n\njulia> c = Dict(:a => [1, 2], :b => 4);\n\njulia> dict_list(c)\n3-element Array{Dict{Symbol,Int64},1}:\n Dict(:a=>1,:b=>4)\n Dict(:a=>2,:b=>4)\n\njulia> c[:model] = \"linear\"; c[:mode] = [\"bi\", \"tri\"];\n\njulia> dict_list(c)\n4-element Array{Dict{Symbol,Any},1}:\n Dict(:a=>1,:b=>4,:mode=>\"bi\",:model=>\"linear\")\n Dict(:a=>2,:b=>4,:mode=>\"bi\",:model=>\"linear\")\n Dict(:a=>1,:b=>4,:mode=>\"tri\",:model=>\"linear\")\n Dict(:a=>2,:b=>4,:mode=>\"tri\",:model=>\"linear\")\n\njulia> c[:e] = [[1, 2], [3, 5]];\n\njulia> dict_list(c)\n8-element Array{Dict{Symbol,Any},1}:\n Dict(:a=>1,:b=>4,:mode=>\"bi\",:e=>[1, 2],:model=>\"linear\")\n Dict(:a=>2,:b=>4,:mode=>\"bi\",:e=>[1, 2],:model=>\"linear\")\n Dict(:a=>1,:b=>4,:mode=>\"tri\",:e=>[1, 2],:model=>\"linear\")\n Dict(:a=>2,:b=>4,:mode=>\"tri\",:e=>[1, 2],:model=>\"linear\")\n Dict(:a=>1,:b=>4,:mode=>\"bi\",:e=>[3, 5],:model=>\"linear\")\n Dict(:a=>2,:b=>4,:mode=>\"bi\",:e=>[3, 5],:model=>\"linear\")\n Dict(:a=>1,:b=>4,:mode=>\"tri\",:e=>[3, 5],:model=>\"linear\")\n Dict(:a=>2,:b=>4,:mode=>\"tri\",:e=>[3, 5],:model=>\"linear\")\n\n\n\n\n\n"
 },
 
 {
-    "location": "savenames/#DrWatson.dict_list_count",
-    "page": "Handling Simulations",
+    "location": "addrun/#DrWatson.dict_list_count",
+    "page": "Running & Listing Simulations",
     "title": "DrWatson.dict_list_count",
     "category": "function",
     "text": "dict_list_count(c) -> N\n\nReturn the number of dictionaries that will be created by calling dict_list(c).\n\n\n\n\n\n"
 },
 
 {
-    "location": "savenames/#Preparing-Simulation-Runs-1",
-    "page": "Handling Simulations",
+    "location": "addrun/#Preparing-Simulation-Runs-1",
+    "page": "Running & Listing Simulations",
     "title": "Preparing Simulation Runs",
     "category": "section",
     "text": "It is very often the case that you want to run \"batch simulations\", i.e. just submit a bunch of different simulations, all using same algorithms and code but just different parameters. This scenario always requires the user to prepare a set of simulation parameter containers which are then passed into some kind of \"main\" function that starts the simulation.To make the preparation part simpler we provide the following functionality:dict_list\ndict_list_countUsing the above function means that you can write your \"preparation\" step into a single dictionary and then let it automatically expand into many parameter containers. This keeps the code cleaner but also consistent, provided that it follows one rule: Anything that is a Vector has many parameters, otherwise it is one parameter. dict_list considers this true irrespectively of what the Vector contains. This allows users to use any iterable custom type as a single \"parameter\" of a simulation.See the Real World Examples for a very convenient application!"
 },
 
 {
-    "location": "savenames/#Simulation-Tables-1",
-    "page": "Handling Simulations",
+    "location": "addrun/#Simulation-Tables-1",
+    "page": "Running & Listing Simulations",
     "title": "Simulation Tables",
     "category": "section",
     "text": "WIP. (Adding simulation runs to a table/csv/dataframe)"
-},
-
-{
-    "location": "savenames/#Produce-or-Load-1",
-    "page": "Handling Simulations",
-    "title": "Produce or Load",
-    "category": "section",
-    "text": "WIP. (loading a simulation or producing it if it doesn\'t exist)"
 },
 
 {
